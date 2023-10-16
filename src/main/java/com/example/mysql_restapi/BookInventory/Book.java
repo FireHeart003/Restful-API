628060fb24364d19c96a25d4c64c82fb65c06db3
madis <madisyn1596@gmail.com>
2023-10-15 22:00:34 -0400

    implemented client package

---
+++ src/main/java/com/example/mysql_restapi/BookInventory/Book.java
@@ -0,0 +1,111 @@
+package com.example.mysql_restapi.BookInventory;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public class Book {
+    @Id
+
+    @SequenceGenerator(
+            name = "book_sequence",
+            sequenceName = "book_sequence",
+            allocationSize = 1
+    )
+
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "book_sequence"
+    )
+    private Long id;
+    private String publisher;
+    private String genre;
+    private double ratings;
+    private int sales;
+    private double discountPercentage;
+    private double price;
+
+    public Book(String publisherB, String s, double v, int i, double v1){
+    }
+
+    public Book(String publisher, String genre, double Ratings, int sales, double discountPercentage, int price
+    ) {
+        this.publisher = publisher;
+        this.genre = genre;
+        this.ratings = ratings;
+        this.sales= sales;
+        this.discountPercentage=discountPercentage;
+        this.price=price;
+    }
+
+    public Book() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public double getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(double ratings) {
+        this.ratings = ratings;
+    }
+
+    public int getSales() {
+        return sales;
+    }
+
+    public void setSales(int sales) {
+        this.sales = sales;
+    }
+
+    public double getDiscountPercentage() {
+        return discountPercentage;
+    }
+
+    public void setDiscountPercentage(double discountPercentage) {
+        this.discountPercentage = discountPercentage;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price){
+        this.price=price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", publisher='" + publisher + '\'' +
+                ", genre='" + genre + '\'' +
+                ", ratings='" + ratings + '\'' +
+                ", sales='" + sales + '\'' +
+                ", discountPercentage='" + discountPercentage + '\'' +
+                '}';
+    }
+}
