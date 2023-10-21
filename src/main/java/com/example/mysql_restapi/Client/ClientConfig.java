c3fe577a8e45e320d17a97c14f0a3753749ed5b2
madis <madisyn1596@gmail.com>
2023-10-21 16:59:54 -0400

    Madisyn commit

---
+++ src/main/java/com/example/mysql_restapi/Client/ClientConfig.java
@@ -0,0 +1,33 @@
+package com.example.mysql_restapi.Client;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class ClientConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(ClientRepository repository){
+        return args -> {
+            com.example.mysql_restapi.Client.Client hello = new Client(
+                    1L,
+                    "hello",
+                    "world"
+            );
+
+            Client fire = new Client(
+                    2L,
+                    "fire",
+                    "water",
+                    "John Doe",
+                    "fire@gmail.com"
+            );
+
+            repository.saveAll(
+                    List.of(hello, fire)
+            );
+        };
+    }
+}
