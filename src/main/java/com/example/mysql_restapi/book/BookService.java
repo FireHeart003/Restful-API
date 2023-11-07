e1fae7804cc5ea75af737983c039cf49c39bb077
madis <madisyn1596@gmail.com>
2023-11-06 19:51:57 -0500

    implemented client package

--- src/main/java/com/example/mysql_restapi/book/BookService.java
+++ src/main/java/com/example/mysql_restapi/book/BookService.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-package com.example.mysql_restapi.book;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
