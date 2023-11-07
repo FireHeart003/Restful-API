153678b14148cc29929c28f92b1279d50241f607
madis <madisyn1596@gmail.com>
2023-11-06 19:49:13 -0500

    implemented client package

--- src/main/java/com/example/mysql_restapi/book/BookService.java
+++ src/main/java/com/example/mysql_restapi/book/BookService.java
@@ -1,5 +1,4 @@
 package com.example.mysql_restapi.book;
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,66 +6,73 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+package com.example.mysql_restapi.book;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.ArrayList;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
-public class BookService
-{
+public class BookService {
     private final BookRepository bookRepository;
 
     @Autowired
-    public BookService(BookRepository bookRepository) {this.bookRepository = bookRepository;}
-    public List<Book> getBook(){ return bookRepository.findAll();}
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 
+    public List<Book> getBooks() {
+        return bookRepository.findAll();
+    }
 
-    public Book retrieveBookByISBN(long isbn)
-    {
-        Optional<Book> bookOptional =
-                bookRepository.FindBookByISBN(isbn);
-        if (bookOptional.isEmpty()){
+    public Book retrieveBookByISBN(long isbn) {
+        Optional<Book> bookOptional = bookRepository.findByISBN(isbn);
+        if (bookOptional.isEmpty()) {
             throw new IllegalStateException("ISBN not found");
         }
-        Book retrieve = null;
-        List<Book> b = bookRepository.findAll();
-        for (int i=0; i<b.size();i++){
-            Book temp = b.get(i);
-
-            if (temp.getISBN() == isbn)
-            {
-                retrieve = temp;
-            }
-        }
-        return retrieve;
+        return bookOptional.get();
     }
 
     public void addNewBook(Book book) {
-        Optional<Book> bookOptional =
-                bookRepository.FindBookByISBN(book.getISBN());
-        if(bookOptional.isPresent()){
-            throw new IllegalStateException("Book exists already.");
+        Optional<Book> bookOptional = bookRepository.findByISBN(book.getISBN());
+        if (bookOptional.isPresent()) {
+            throw new IllegalStateException("Book with the same ISBN already exists.");
         }
         bookRepository.save(book);
     }
 
-
-    //BROWSING AND SORTING
     public List<Book> getBooksByGenre(String genre) {
         return bookRepository.findByGenre(genre);
     }
+
     public List<Book> getBooksByRating(double rating) {
-        return bookRepository.findByRatesGreaterThanEqual(rating);
+        return bookRepository.findByRatingsGreaterThanEqual(rating);
     }
-    public List<Integer> getTopSellers() {
-        return bookRepository.findTop10ByOrderByCopiesSoldDesc();
+
+    public List<TopSeller> getTopSellers() {
+        List<TopSeller> topSellers = bookRepository.findTop10ByOrderByCopiesSoldDesc();
+        List<TopSeller> topSellerList = new ArrayList<>();
+
+        for (TopSeller book : topSellers) {
+            topSellerList.add(new TopSeller(book.getTitle(), book.getCopiesSold()));
+        }
+
+        return topSellerList;
     }
+
+
     @Transactional
     public void discountBooksByPublisher(String publisher, double discountPercentage) {
         List<Book> books = bookRepository.findByPublisher(publisher);
         for (Book book : books) {
-            double currentPrice = book.getPrice(); // You need to have a price attribute in your Book entity
+            double currentPrice = book.getPrice();
             double discountedPrice = currentPrice * (1.0 - discountPercentage / 100.0);
             book.setPrice(discountedPrice);
             bookRepository.save(book);
         }
     }
-
 }
